cb121bcc62c874b3c28255b821c5d03a4be75d5c
Evan <evanyou2004@gmail.com>
2023-08-17 20:07:12 -0500

    created function getComputerChoice() that returns a random choice.

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("hello world!");
+const choices = ["Rock" , "Paper" , "Scissors"];
+
+function randInt(min, max) {
+    return Math.floor(Math.random() * max) + min;   
+}
+
+function getComputerChoice() {
+    return choices[randInt(0 , choices.length)];
+}
+
+// console.log(getComputerChoice())
