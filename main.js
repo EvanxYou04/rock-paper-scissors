aa85ef99da5d11cadae8946768252180ad0d666f
Evan <evanyou2004@gmail.com>
2023-08-18 23:13:53 -0500

    created getPlayerChoice(). added ; to terminate lines in playRound().

--- main.js
+++ main.js
@@ -17,23 +17,44 @@ function playRound(playerSelection, computerSelection) {
         console.log("tie");
     }
     else if (playerSelection == "Rock" && computerSelection == "Paper" ) {
-        console.log("computer wins!")
+        console.log("computer wins!");
     }
     else if (computerSelection == "Rock" && playerSelection == "Paper" ) {
-        console.log("player wins!")
+        console.log("player wins!");
     }
     else if (computerSelection == "Scissors" && playerSelection == "Paper" ) {
-        console.log("computer wins!")
+        console.log("computer wins!");
     }
     else if (playerSelection == "Scissors" && computerSelection == "Paper" ) {
-        console.log("player wins!")
+        console.log("player wins!");
     }
     else if (playerSelection == "Rock" && computerSelection == "Scissors" ) {
-        console.log("player wins!")
+        console.log("player wins!");
     }
     else if (computerSelection == "Rock" && playerSelection == "Scissors" ) {
-        console.log("computer wins!")
+        console.log("computer wins!");
     }
 }
+
+function getPlayerChoice() {
+    let playerChoice = prompt("Select your choice (Rock,Paper,Scissors): ");
+    switch (playerChoice.toLowerCase) {
+        case "rock":
+            playerChoice = choices[0];
+            break;
+
+        case "paper":
+            playerChoice = choices[1];
+            break;
+
+        case "scissors":
+            playerChoice = choices[2];
+            break;
+        default:
+            console.log("incorrect input. try again")
+            getPlayerChoice();
+            break;
+    } 
+}   
 
 // console.log(getComputerChoice())
